bfc88f102f960fde96b699e55ae63a6fb1b403a7
Mwesige256 <mwesigemuka@gmail.com>
2026-06-05 18:51:35 +0400

    Add polyfill for Array.prototype.toReversed
    Added polyfill for Array.prototype.toReversed for compatibility with older Node versions.

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,12 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
+// Polyfill for older Node versions
+if (!Array.prototype.toReversed) {
+  Array.prototype.toReversed = function() {
+    return this.slice().reverse();
+  };
+}
+
 const config = getDefaultConfig(__dirname);
 module.exports = withNativeWind(config, { input: "./global.css" });
